293b0d2dbc7316891545be026b829e626e420acf
fenz050896 <michaelfitrah@yahoo.com>
2022-05-28 22:52:49 +0700

    must use next/link

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 function Nav() {
   const [active, setActive] = useState(false);
@@ -8,10 +9,18 @@ function Nav() {
     <nav>
       <ul className={`menu ${active ? 'active' : ''}`}>
         <li className="logo"><a href="#">Company Name</a></li>
-        <li className={`menu__item ${router.pathname === '/' ? 'menu__item__active' : ''}`}><a href="/">Product</a></li>
-        <li className={`menu__item ${router.pathname === 'a' ? 'menu__item__active' : ''}`}><a href="#">About</a></li>
-        <li className={`menu__item ${router.pathname === 'b' ? 'menu__item__active' : ''}`}><a href="#">Blogs</a></li>
-        <li className={`menu__item ${router.pathname === 'c' ? 'menu__item__active' : ''}`}><a href="#">Contact</a></li>
+        <li className={`menu__item ${router.pathname === '/' ? 'menu__item__active' : ''}`}>
+          <Link href="/"><a>Product</a></Link>
+        </li>
+        <li className={`menu__item ${router.pathname === 'a' ? 'menu__item__active' : ''}`}>
+          <Link href="#"><a>About</a></Link>
+        </li>
+        <li className={`menu__item ${router.pathname === 'b' ? 'menu__item__active' : ''}`}>
+          <Link href="#"><a>Blogs</a></Link>
+        </li>
+        <li className={`menu__item ${router.pathname === 'c' ? 'menu__item__active' : ''}`}>
+          <Link href="#"><a>Contact</a></Link>
+        </li>
         <li className="toggle" onClick={() => { setActive(!active); }}>
           <a>{ active ? <i className="fa fa-times fa-2"></i> : <i className="fa fa-bars fa-2"></i> }</a>
         </li>
